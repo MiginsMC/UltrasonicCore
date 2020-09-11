d5aff301438e90aa1a5ca18033b95173315b601b
MiginsMC <thefiregod2511@gmail.com>
2020-09-11 18:00:16 +1000

    Added embeds and edits

--- src/main/java/me/mig/ultrasoniccore/UltrasonicCore.java
+++ src/main/java/me/mig/ultrasoniccore/UltrasonicCore.java
@@ -4,20 +4,27 @@ import me.mig.ultrasoniccore.commands.Reload;
 import me.mig.ultrasoniccore.commands.Suicide;
 import me.mig.ultrasoniccore.commands.Teleport;
 import me.mig.ultrasoniccore.events.SleepEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
+import org.apache.commons.lang.math.RandomUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.time.Instant;
 import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
 
 public class UltrasonicCore extends JavaPlugin {
 
     public static JDA jda;
+
 
     @Override
     public void onEnable() {
@@ -28,6 +35,7 @@ public class UltrasonicCore extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new SleepEvent(), this);
 
         if (getBot()) {
+
 
             Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
                 StringBuilder online = new StringBuilder();
@@ -41,14 +49,33 @@ public class UltrasonicCore extends JavaPlugin {
                 }
 
                 TextChannel tc = jda.getTextChannelById("748466123548393475");
+
+                // Apparently this does nothing lol
                 assert tc != null;
-                if (Bukkit.getOnlinePlayers().size() > 0) {
-                    tc.sendMessage("There are " + Bukkit.getOnlinePlayers().size() + " players online right now. \n"
-                            + online.toString()).queue();
-                } else {
-                    tc.sendMessage("There are no players online right now ;(").queue();
-                }
-            }, 200L, 200L);
+
+                Consumer<List<Message>> callback = (response) -> {
+                    System.out.println(response);
+                    Message contains = null;
+                    for (Message m : response) {
+                        if (m.getEmbeds().size() > 0) {
+                            System.out.println("Found an embed :O");
+                            contains = m;
+                            break;
+                        }
+                    }
+                    if (contains != null) {
+                        System.out.println("Editing");
+                        contains.editMessage(getEmbed(online)).queue();
+                    } else {
+                        System.out.println("Sending");
+                        tc.sendMessage(getEmbed(online)).queue();
+                    }
+
+                };
+                // Executes callback, above happens after the messages have been retrieved.
+                tc.getHistory().retrievePast(30).queue(callback);
+
+            }, 100L, 2000L);
         } else {
             System.out.println("Bot didn't start :(");
         }
@@ -77,5 +104,19 @@ public class UltrasonicCore extends JavaPlugin {
                 e.printStackTrace();
                 return false;
             }
+    }
+
+    public MessageEmbed getEmbed(StringBuilder online) {
+        EmbedBuilder eb = new EmbedBuilder();
+        int players = Bukkit.getOnlinePlayers().size();
+
+
+        eb.setTitle("Server Stats")
+        .setColor(Color.CYAN)
+        .addField("Players Online (" + players + ")", players > 0 ? online.toString() : "No ones here :(", true)
+//        eb.addField("TPS (Ticks Per Second)", this.getServer().ticksper)
+        .setFooter("caryl sees all", jda.getSelfUser().getAvatarUrl())
+        .setTimestamp(Instant.now());
+       return eb.build();
     }
 }
